5422b2918384780d667e877d6b536e44fcbed9f3
Deasoso <932152571@qq.com>
2020-12-13 00:41:16 +0800

    finish.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
+    <Globe 
+      v-model="activeCountryCode" 
+    />
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import Globe from '@/components/Globe.vue';
+
+export default {
+  name: 'globe-view',
+  components: {
+    Globe,
+  },
+  data() {
+    return {
+      activeCountryCode: null
+    };
+  },
+}
+</script>
